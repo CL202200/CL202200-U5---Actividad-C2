12f6f9e067b07e5eb436cc7319a9a55341296dff
CL202200 <119093111+CL202200@users.noreply.github.com>
2022-11-26 00:38:36 -0600

    Add files via upload

---
+++ resources/tempCodeRunnerFile.java
@@ -0,0 +1,17 @@
+package fca.unam.mx.resources;
+
+public class ClientsResource {
+@GET
+    @Path("/all")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Operation(summary = "Get all products")
+    @APIResponses(value = {
+            @APIResponse(responseCode = "200", content = @Content(mediaType = MediaType.APPLICATION_JSON)),
+    })
+    public Response getProducts() {
+
+        var responseDto = storeDal.getProducts();
+        return Response.ok(responseDto).build();
+    }
+    
+}
